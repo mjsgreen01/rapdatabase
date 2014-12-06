10330213751734efeddd8df3273e563f7a80259b
mjsgreen01 <mjsgreen01@gmail.com>
2014-12-06 18:15:29 -0500

    get artist, album, and song url functions created, and save each to json files

--- capybaraTest.rb
+++ capybaraTest.rb
@@ -15,25 +15,41 @@ class SearchResultParser
 	def scrollEntirePage(url)
 		session.visit url
 
-		while session.first(:css, ".pagination") != nil
-			session.execute_script("scrollTo(0,document.body.scrollHeight);")
-			sleep 1
-		end
+		# while session.first(:css, ".pagination") != nil
+		# 	session.execute_script("scrollTo(0,document.body.scrollHeight);")
+		# 	sleep 1
+		# end
 		return self
 	end
 
+	def artistAlbumUrlArray(u)
+			session.visit u
 
+			if session.first(:css, ".album_link") != nil
+				urls = session.all(:css, ".album_link")
+				return urls.map {|u| u[:href]}
+			else
+				return nil
+			end
+	end
 
-	def urlArray
+	def albumSongUrlArray(u)
+		session.visit u
 
-		urls = session.all(:css, ".search_result a")
-		return urlsArray = urls.map {|u| u[:href]}
+		urls = session.all(:css, ".song_name")
+		return urls.map {|u| u[:href]}
+	end
+
+	def artistUrlArray
+
+		urls = session.all(:css, ".artist_link")
+		return urls.map {|u| u[:href]}
 	end
 
 	def parseArray(url)
 		session.visit url
 
-		theUrls = urlArray
+		theUrls = artistUrlArray
 		theUrls.map{|u| puts $parser.urlSearch(u)}
 
 	end
@@ -94,7 +110,20 @@ $parser = PageParser.new(session)
 arrayParser = SearchResultParser.new(session)
 
 # arrayParser.parseArray("http://genius.com/search?q=outkast")
-arrayParser.scrollEntirePage("http://genius.com/search?q=outkast")
+artistUrls = arrayParser.scrollEntirePage("http://genius.com/artists").artistUrlArray
+puts artistUrls
+artistUrls_json = JSON.pretty_generate(artistUrls)
+File.write("artistUrls.json", artistUrls_json)
+
+albumUrls = artistUrls.map{|u| arrayParser.artistAlbumUrlArray(u)}.select{|i| i}
+puts albumUrls
+albumUrls_json = JSON.pretty_generate(albumUrls)
+File.write("albumUrls.json", albumUrls_json)
+
+songUrls = albumUrls.map{|u| u.map{|i| arrayParser.albumSongUrlArray(i)}}
+puts songUrls
+songUrls_json = JSON.pretty_generate(songUrls)
+File.write("songUrls.json", songUrls_json)
 
 # puts parser.urlSearch("http://genius.com/2pac-got-my-mind-made-up-lyrics")
 
