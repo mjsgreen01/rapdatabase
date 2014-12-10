a22f3cc1b940c3ea70c38c2ff6f85e40d7ecbc63
mjsgreen01 <mjsgreen01@gmail.com>
2014-12-09 22:10:23 -0500

    everything before creating D3 json files

--- hiphopCollabApp/app/models/artist.rb
+++ hiphopCollabApp/app/models/artist.rb
@@ -3,5 +3,31 @@ class Artist < ActiveRecord::Base
 	has_many :song_involvements
 	has_many :songs, through: :song_involvements
 
+	def collaborators
+		collabsArray = self.songs.map{|i| i.artists}.flatten
+		collabsArray.map{|i| i.name}
+	end
+
+
+
+	def structureNodesJson
+		artists_array = []
+		Artist.all.each{|i| n = i.name
+			artists_array.push n
+		}
+
+		nodes = artists_array.map{|name|  {"name" => name, "group" => 1 }  }
+		links = []
+		artists_array.each{|i|
+			collabs = i.collaborators
+			collabsNames = collabs.map{|c| c.name}
+			collabsNames.each{|n|
+				links.push({"source"=>artists_array.index(i), "target"=>artists_array.index(n), "value"=>2})
+			}
+		}
+	end
+
+
+
 
 end
--- hiphopCollabApp/db/seeds.rb
+++ hiphopCollabApp/db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'json'
+require 'pry'
+
+Artist.destroy_all
+Song.destroy_all
+SongInvolvement.destroy_all
+
+
+def import_file(file_name)
+	data = File.read(file_name)
+	songs = JSON.parse(data)
+	songs.flatten!
+
+	songs.each do |song_hash|
+		next unless song_hash
+		main_artist = Artist.find_or_create_by(name: song_hash["artist"])
+		song = Song.create(title: song_hash["title"], song_link: song_hash["audioLink"])
+		song.song_involvements.create(artist: main_artist, primary: true)
+
+		if song_hash["featured"]
+			song_hash["featured"].each do |feat_artist_name|
+				feat_artist = Artist.find_or_create_by(name: feat_artist_name)
+				song.song_involvements.create(artist: feat_artist, featured: true)
+			end
+		end
+		if song_hash["producer"]
+			song_hash["producer"].each do |producer_artist_name|
+				producer_artist = Artist.find_or_create_by(name: producer_artist_name)
+				song.song_involvements.create(artist: producer_artist, producer: true)
+			end
+		end
+	end
+
+	
+end
+
+(1..9).each do |i|
+	(0..59).each do |j|
+		begin
+			import_file("#{Rails.root}/db/data/songData#{i}_#{j}.json")
+		rescue Exception => e
+			Rails.logger.error(e)
+		end	
+	end
+end
